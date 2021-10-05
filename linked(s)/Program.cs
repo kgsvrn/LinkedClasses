e6201afa7011a7a91bb242ced4783f95eaf61154
kgsvrn <90982069+kgsvrn@users.noreply.github.com>
2021-10-05 16:04:40 +0300

    1,2,3

---
+++ linked(s)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace linked_s_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> one = new LinkedList<int>();
+            one.Append(9);
+            one.Append(11);
+            one.Append(93);
+            one.Append(1221);
+            LinkedList<string> two = new LinkedList<string>();
+            two.Append("safasdfas");
+            two.Append("ffffff");
+            two.Append("qwerty");
+            two.Append("wwwww");
+
+            foreach (var i in one)
+            {
+               
+                Console.WriteLine(i);
+            }
+            foreach (var i in two)
+            {
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ linked(s)/elems.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linked_s_
+{
+
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        public class Node<T>
+        {
+         
+            public Node<T> Next = null;
+          
+            public T Data;
+        }
+
+        private Node<T> root = null;
+
+        public Node<T> First { get { return root; } }
+
+        public Node<T> Last
+        {
+            get
+            {
+                Node<T> curr = root;
+                if (curr == null)
+                    return null;
+                while (curr.Next != null)
+                    curr = curr.Next;
+                return curr;
+            }
+        }
+
+        public T PrintLast { get { return Last.Data; }  }
+        public T PrintFirst { get { return First.Data; } }
+
+        public void Append(T value)
+        {
+            Node<T> n = new Node<T> { Data = value };
+            if (root == null)
+                root = n;
+            else
+                Last.Next = n;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = root;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+
+
+}
+    
+
